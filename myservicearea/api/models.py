1245719813c1fa842e9ed29c59e1e46d998e3268
Clederson Dotti <cledersonsj@gmail.com>
2024-05-30 18:39:51 -0300

    updating the area field used for polygon info - making use of django gis to simplify operations

--- myservicearea/api/models.py
+++ myservicearea/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from djgeojson.fields import PolygonField
+from django.contrib.gis.db.models.fields import PolygonField
 
 
 class Provider(models.Model):
@@ -18,7 +18,7 @@ class Provider(models.Model):
 class ServiceArea(models.Model):
     name = models.CharField(max_length=100, blank=False, default = '')
     price = models.FloatField()
-    polygon = PolygonField()
+    area = PolygonField(default=None)
     information = models.CharField(max_length=100, blank=False, default = '')
     active = models.BooleanField()
     provider = models.ForeignKey(Provider, 
